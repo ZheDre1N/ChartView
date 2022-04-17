8f0e06f3bda77f2840d14f0e16f920ec802c8eef
Eugene Dudkin <ZheDre1N@gmail.com>
2022-04-17 12:46:22 +0300

    ChartView moved from My portfolio app

--- Package.swift
+++ Package.swift
@@ -21,8 +21,5 @@ let package = Package(
         .target(
             name: "ChartView",
             dependencies: []),
-        .testTarget(
-            name: "ChartViewTests",
-            dependencies: ["ChartView"]),
     ]
 )
